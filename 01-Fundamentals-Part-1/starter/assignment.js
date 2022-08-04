202a7cd088c6962a5bd2b4dd19b47c11fa8aee71
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-08-04 14:06:51 -0400

    Add assignment.js and script.js. Complete values and variables assignment

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,5 @@
+let country = 'United States of America';
+let continent = 'North America';
+let population = 330000000;
+
+console.log(country, continent, population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,5 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+// this is how to declare a variable and assign it a value.
+let firstName = 'Jonas';
