8377ce7645c92fff17deedec27775d952473eaf9
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-08-06 15:41:37 -0400

    complete first coding challenge

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,5 +1,23 @@
+// Values and Variables Assignment
 let country = 'United States of America';
 let continent = 'North America';
 let population = 330000000;
+// console.log(country, continent, population);
 
-console.log(country, continent, population);
+// Data types Assignment
+let isIsland = false;
+let language;
+// console.log(isIsland, population, country, language);
+
+// coding challenge #1
+let johnsMass, marksMass;
+function BMI(mass, heigth) {
+  return mass / heigth ** 2;
+}
+
+johnsMass = BMI(92, 1.95);
+marksMass = BMI(78, 1.69);
+
+const markHigherBMI = marksMass > johnsMass;
+
+console.log(johnsMass, marksMass, markHigherBMI);
