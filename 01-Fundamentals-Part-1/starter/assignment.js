897c1446c455055e7d177368a91759a3239637b1
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-08-06 20:30:11 -0400

    complete coding challenge 2

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -9,15 +9,25 @@ let isIsland = false;
 let language;
 // console.log(isIsland, population, country, language);
 
-// coding challenge #1
+// TODO: Coding challenge #1
 let johnsMass, marksMass;
+
 function BMI(mass, heigth) {
   return mass / heigth ** 2;
 }
 
 johnsMass = BMI(92, 1.95);
 marksMass = BMI(78, 1.69);
-
 const markHigherBMI = marksMass > johnsMass;
+// console.log(johnsMass, marksMass, markHigherBMI);
 
-console.log(johnsMass, marksMass, markHigherBMI);
+//TODO: Coding Challenge 2
+
+if (johnsMass > marksMass)
+  console.log(
+    `John's BMI of ${johnsMass} is higher than Mark's BMI of ${marksMass}`
+  );
+else
+  console.log(
+    `Mark's BMI of ${marksMass} is higher than John's BMI of ${johnsMass}`
+  );
