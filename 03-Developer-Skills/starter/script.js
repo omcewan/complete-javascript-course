9d7644877cc948e4c9f65cf3b40751b3c6966765
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-11-25 17:29:21 -0500

    completed Developer Skills section

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,19 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+// TODO: Coding Challenge 1
+
+let data1 = [17, 21, 23];
+let data2 = [12, 5, -5, 0, 4];
+
+function printForecast(temps) {
+  let foreCast = '...';
+
+  for (let i = 0; i < temps.length; i++) {
+    foreCast += `${temps[i]}°C in ${i + 1} days...`;
+  }
+
+  console.log(foreCast);
+}
+printForecast(data1);
+printForecast(data2);
