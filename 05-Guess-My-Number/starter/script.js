8a00558d9f3287d595688322b6b3059382c5f0b5
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-12-23 22:58:53 -0500

    refactor guess my number game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,58 +11,55 @@ const messageElement = document.querySelector('.message');
 const numberElement = document.querySelector('.number');
 const guessElement = document.querySelector('.guess');
 
+function displayMessage(message) {
+  messageElement.textContent = message;
+}
+
 function checkInput() {
   const guess = Number(guessElement.value);
 
   // check if there is an input and not we change the game message board
   if (!guess) {
-    messageElement.textContent = '⛔️ No Number!';
-
+    displayMessage('⛔️ No Number!');
     // if the guess is equal to the number
   } else if (guess === number) {
     document.querySelector('body').style.backgroundColor = '#60b347';
     numberElement.textContent = number;
     numberElement.style.width = '30rem';
-    messageElement.textContent = '🥳 Correct Number!';
+    displayMessage('🥳 Correct Number!');
     // check if new score is higher than highscore and then save it as the new highscore
     if (highscore < score) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
-  } else if (guess > number) {
-    guessTooHigh();
-  } else if (guess < number) {
-    guessTooLow();
+  } else if (guess != number) {
+    guessNotNumber(guess);
   }
 }
 
-function guessTooHigh() {
+function guessNotNumber(guess) {
   if (score > 1) {
-    messageElement.textContent = '📈 Too High!';
+    guess > number
+      ? displayMessage('📈 Too High!')
+      : displayMessage('📉 Too Low!');
     score--;
     scoreElement.textContent = score;
   } else {
-    messageElement.textContent = '💥 You Lost!';
-    scoreElement.textContent = 0;
-  }
-}
-
-function guessTooLow() {
-  if (score > 1) {
-    messageElement.textContent = '📉 Too Low!';
-    score--;
-    scoreElement.textContent = score;
-  } else {
-    messageElement.textContent = '💥 You Lost!';
+    displayMessage('💥 You Lost!');
     scoreElement.textContent = 0;
   }
 }
 
 document.querySelector('.check').addEventListener('click', checkInput);
+guessElement.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    checkInput();
+  }
+});
 
 document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
-  messageElement.textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   numberElement.style.width = '15rem';
   number = Math.trunc(Math.random() * 20) + 1;
   // console.log(number);
